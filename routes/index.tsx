0773e2df2c59beb8de508eb3685ee0e2e8726311
sereinne <akbarpersonalmode@gmail.com>
2025-07-06 00:51:30 +0700

    change theme of bio, added more icons, adjust icons to match the corresponding theme.

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,22 +5,20 @@ export default function Home() {
     url: string;
   };
 
-  const technologies: Array<technology> = [
+  const frameworks: Array<technology> = [
     {
-      iconPath: "/linux.svg",
-      desc: "Linux",
-      url: "https://github.com/torvalds/linux",
+      iconPath: "/logo.svg",
+      desc: "Fresh",
+      url: "https://fresh.deno.dev",
     },
     {
-      iconPath: "/nodejs.svg",
-      desc: "NodeJS",
-      url: "https://nodejs.org",
+      iconPath: "/tailwindcss.svg",
+      desc: "TailwindCSS",
+      url: "https://tailwindcss.com",
     },
-    {
-      iconPath: "/deno.svg",
-      desc: "Deno",
-      url: "https://deno.com",
-    },
+  ];
+
+  const languages: Array<technology> = [
     {
       iconPath: "/rust.svg",
       desc: "Rust",
@@ -35,16 +33,6 @@ export default function Home() {
       iconPath: "/zig.svg",
       desc: "Zig",
       url: "https://ziglang.org/",
-    },
-    {
-      iconPath: "/bash.svg",
-      desc: "Bash",
-      url: "https://ftp.gnu.org/gnu/bash",
-    },
-    {
-      iconPath: "/git.svg",
-      desc: "Git",
-      url: "https://git-scm.com",
     },
     {
       iconPath: "/js.svg",
@@ -66,6 +54,35 @@ export default function Home() {
       desc: "CSS",
       url: "https://w3schools.com",
     },
+  ];
+
+  const developerTools: Array<technology> = [
+    {
+      iconPath: "/linux.svg",
+      desc: "Linux",
+      url: "https://github.com/torvalds/linux",
+    },
+    {
+      iconPath: "/deno.svg",
+      desc: "Deno",
+      url: "https://deno.com",
+    },
+    {
+      iconPath: "/nodejs.svg",
+      desc: "NodeJS",
+      url: "https://nodejs.org",
+    },
+
+    {
+      iconPath: "/bash.svg",
+      desc: "Bash/zsh/fish",
+      url: "https://ftp.gnu.org/gnu/bash",
+    },
+    {
+      iconPath: "/git.svg",
+      desc: "Git",
+      url: "https://git-scm.com",
+    },
     {
       iconPath: "/docker.svg",
       desc: "Docker",
@@ -77,16 +94,16 @@ export default function Home() {
     <div className="homepage bg-[#EDCB96]">
       <header>
         <video autoplay loop>
-          <source src="/output.mp4" />
+          <source src="/new-output.mp4" />
         </video>
       </header>
-      <main class="bg-[#EDCB96] mx-4 my-2 font-montserrat grid grid-cols-3 gap-y-1 gap-x-3">
+      <main class="bg-[#EDCB96] mx-4 my-10 font-montserrat grid grid-cols-3 gap-y-9 gap-x-3">
         <div className="introduction">
           <header class="font-bold text-4xl text-center">
             Brief introduction
           </header>
           <hr class="bg-[#524632] h-0.5 rounded-md border-0 my-2 mx-1" />
-          <p class="text-center text-lg font-semibold leading-tight">
+          <p class="text-center text-md font-semibold leading-tight">
             My name is Muhammad Akbar Ilman Setijadi. I'm 19 years old, majoring
             Information Systems at Institut Teknologi Sepuluh Nopember (10th
             Nopember Institute of Technology), Surabaya. I'm Currently refining
@@ -98,30 +115,72 @@ export default function Home() {
             Projects
           </header>
           <hr class="bg-[#524632] h-0.5 rounded-md border-0 my-2 mx-1" />
-          <div className="projects-grid flex flex-col gap-y-2">
-            <div className="project-grid rounded-md border-4 border-[#524632]">
-              <header class="text-center font-semibold text-[#EDCB96] bg-[#524632] py-1">
-                Szlog
-              </header>
-              <p class="text-center ">
-                simple structured logging library in zig
-              </p>
-            </div>
-          </div>
         </div>
-        <div className="tech-stacks">
+        <div className="contributions">
           <header class="font-bold text-4xl text-center">
-            Technologies
+            Contributions
           </header>
           <hr class="bg-[#524632] h-0.5 rounded-md border-0 my-2 mx-1" />
-          <div className="tech-stack-grid grid gap-2 grid-cols-3">
-            {technologies.map((tech) => {
+        </div>
+        <div className="dev-tools">
+          <header class="font-bold text-4xl text-center">
+            Developer Tools
+          </header>
+          <hr class="bg-[#524632] h-0.5 rounded-md border-0 my-2 mx-1" />
+          <div className="dev-tools-grid grid gap-2 grid-cols-3">
+            {developerTools.map((devtool) => {
               return (
-                <a href={tech.url}>
-                  <div className="tech-stack flex flex-col justify-center align-center gap-x-2 border-4 rounded-md border-[#524632] duration-200 hover:bg-[#468c49] hover:text-[#EDCB96] hover:-translate-y-1">
-                    <img src={tech.iconPath} class="size-8 mx-auto my-0.5" />
+                <a href={devtool.url}>
+                  <div className="dev-tool flex flex-col justify-center align-center gap-x-2 border-4 rounded-md border-[#524632] duration-200 hover:bg-[#468c49] hover:text-[#EDCB96] hover:-translate-y-1">
+                    <img src={devtool.iconPath} class="size-8 mx-auto my-0.5" />
                     <footer class="text-center font-semibold bg-[#524632] text-[#edcb96]">
-                      {tech.desc}
+                      {devtool.desc}
+                    </footer>
+                  </div>
+                </a>
+              );
+            })}
+          </div>
+        </div>
+        <div className="programming-languages">
+          <header class="font-bold text-4xl text-center">
+            Languages
+          </header>
+          <hr class="bg-[#524632] h-0.5 rounded-md border-0 my-2 mx-1" />
+          <div className="programming-languages-grid grid gap-2 grid-cols-3">
+            {languages.map((language) => {
+              return (
+                <a href={language.url}>
+                  <div className="programming-language flex flex-col justify-center align-center gap-x-2 border-4 rounded-md border-[#524632] duration-200 hover:bg-[#468c49] hover:text-[#EDCB96] hover:-translate-y-1">
+                    <img
+                      src={language.iconPath}
+                      class="size-8 mx-auto my-0.5"
+                    />
+                    <footer class="text-center font-semibold bg-[#524632] text-[#edcb96]">
+                      {language.desc}
+                    </footer>
+                  </div>
+                </a>
+              );
+            })}
+          </div>
+        </div>
+        <div className="frameworks">
+          <header class="font-bold text-4xl text-center">
+            Frameworks
+          </header>
+          <hr class="bg-[#524632] h-0.5 rounded-md border-0 my-2 mx-1" />
+          <div className="frameworks-grid grid gap-2 grid-cols-3">
+            {frameworks.map((framework) => {
+              return (
+                <a href={framework.url}>
+                  <div className="framework-grid flex flex-col justify-center align-center gap-x-2 border-4 rounded-md border-[#524632] duration-200 hover:bg-[#468c49] hover:text-[#EDCB96] hover:-translate-y-1">
+                    <img
+                      src={framework.iconPath}
+                      class="size-8 mx-auto my-0.5"
+                    />
+                    <footer class="text-center font-semibold bg-[#524632] text-[#edcb96]">
+                      {framework.desc}
                     </footer>
                   </div>
                 </a>
