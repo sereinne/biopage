54efff70d4292fe6d30f685e985a1ac10d46882e
sereinne <akbarpersonalmode@gmail.com>
2025-12-22 13:04:46 +0700

    added calligraphy.

--- src/index.tsx
+++ src/index.tsx
@@ -119,7 +119,8 @@ function Introduction() {
   return (
     <main class="portfolio bg-sleek-gray h-screen flex flex-col">
       <div class="introduction flex flex-col justify-center items-center my-auto">
-        <div class="introduction-heading flex flex-col items-center gap-y-8">
+        <div class="introduction-heading flex flex-col items-center gap-y-5">
+          <img src="/src/assets/calligraphy.svg" class="w-96 h-96" />
           <div class="intro-wrapper-waving-hand flex gap-x-5 items-center">
             <p class="text-sleek-red text-8xl font-bold font-sleek text-center">
               Hi!
